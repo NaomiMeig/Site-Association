5f5c9256c75d799d2948e7d88a76b7722df81580
NaomiMeig <akakponaomi0@gmail.com>
2025-04-15 14:47:03 +0100

    mdiff

--- src/components/phase2/Detail.jsx
+++ src/components/phase2/Detail.jsx
@@ -3,7 +3,6 @@ function Detail (){
     return <div>
         <h2 className="text-cyan-300 text-3xl sticky font-bold">Our association</h2><br/>
         <h2 className="text-2xl sticky font-bold">Association Qualification des Jeunes</h2>
-        <h2 classsName="text-2xl sticky font-bold">Association qualification des jeunes </h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
             A, quaerat dolor placeat eveniet consectetur inventore nihil eum 
              quas hic cum illum. Ipsam, ad est! Aliquid harum omnis minima soluta!</p>
